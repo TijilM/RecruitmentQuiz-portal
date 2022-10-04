48c28807663f87ad39804240f818cd10fdf8aa40
sanya-mahajan <sanyamahajan08@gmail.com>
2022-10-04 19:38:52 +0530

    footer added

---
+++ frontend/src/Pages/Home/Components/Footer/index.js
@@ -0,0 +1,36 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faFacebook,faLinkedin,faYoutube,faInstagram,faDiscord} from "@fortawesome/free-brands-svg-icons"
+
+
+import {FooterContainer,FooterWrap,FooterLinksContainer,FooterLinksWrapper,FooterLinkTitle} from './FooterElements'
+import{SocialIcons,SocialMedia} from './FooterElements'
+
+function Footer(){
+    return (
+        <FooterContainer>
+        <FooterWrap>
+        <FooterLinksContainer>
+        <FooterLinksWrapper>
+        
+             <FooterLinkTitle>Connect With Us &hearts; 
+             </FooterLinkTitle>
+            
+        
+        </FooterLinksWrapper></FooterLinksContainer>
+        <FooterLinksWrapper>
+        <SocialMedia>
+        
+              
+                 <SocialIcons>
+                 <a className="social-button facebook" href="https://www.facebook.com/CCSTU/?ref=br_rs" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>
+                 <a className="social-button linkedin" href="https://www.linkedin.com/company/ccs-tiet/" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+                 <a className="social-button youtube" href="https://youtube.com/channel/UCc-F6rlsDdHAKfSiPJEnDLg" target="_blank"><FontAwesomeIcon icon={faYoutube} /></a>
+                 <a className="social-button instagram" href="http://instagram.com/ccs_tiet?utm_source=qr" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
+                 </SocialIcons>
+                
+        </SocialMedia>
+        </FooterLinksWrapper></FooterWrap></FooterContainer>
+    )
+}
+
+export default Footer
--- frontend/src/Pages/Home/index.js
+++ frontend/src/Pages/Home/index.js
@@ -2,7 +2,7 @@ import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero";
 import WhyCCS from "./Components/WhyCCS"
 import Carousel from "./Components/Carousel"
-import Footer from "./Components/Footer"
+import Footer from "./Components/Footer/index"
 
 import styles from "./Style/home.module.css"
 
