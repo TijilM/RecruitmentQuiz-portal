9e6ad49784ba043b26da88672773c5d81768e55e
Aitchessbee <bediharsiddak@gmail.com>
2022-10-05 11:30:08 +0530

    working on test portal

--- frontend/src/Pages/Test/Components/LeftAside.js
+++ frontend/src/Pages/Test/Components/LeftAside.js
@@ -1,20 +1,22 @@
+import styles from "../Style/test.module.css"
+
 function LeftAside(){
     return (
         <div>
-            <img src={require("../Assets/Traced-Logo.png")} alt="ccsLogo" className="test-logo"/>
-            <hr className="test-leftAside-line"/>
-            <div className="test-user">
-                <div className="test-name">ADITYA PARMAR</div>
-                <div className="test-email">ADITYAPARMAR@GMAIL.COM</div>
+            <img src={require("../Assets/Traced-Logo.png")} alt="ccsLogo" className={styles.testLogo} />
+            <hr className={styles.testLeftAsideLine}/>
+            <div className={styles.testUser}>
+                <div className={styles.testName}>ADITYA PARMAR</div>
+                <div className={styles.testEmail}>ADITYAPARMAR@GMAIL.COM</div>
             </div>
 
-            <div className="test-leftAside-bottom">
-                <div className="test-contact">
-                    <div className="test-name">CONTACT US</div>
-                    <div className="test-email">TECHSUPPORT@GMAIL.COM</div>
+            <div className={styles.testLeftAdieBottom}>
+                <div className={styles.testContact}>
+                    <div className={styles.testName}>CONTACT US</div>
+                    <div className={styles.testEmail}>TECHSUPPORT@GMAIL.COM</div>
                 </div>
-                <hr className="test-leftAside-line"/>
-                <button className="test-logout">LOGOUT</button>
+                <hr className={styles.testLeftAsideLine}/>
+                <button className={styles.testLogout}>LOGOUT</button>
             </div>
         </div>
     )
