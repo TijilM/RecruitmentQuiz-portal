4f888d7355a621f50cd646529aca994fe8019626
Balerion123 <adityaparmar9815@gmail.com>
2022-10-07 23:04:26 +0530

    small spacing changes

--- frontend/src/Pages/Home/Components/Footer/FooterElements.js
+++ frontend/src/Pages/Home/Components/Footer/FooterElements.js
@@ -1,62 +1,52 @@
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-
-
-export const FooterContainer=styled.footer`
+export const FooterContainer = styled.footer`
   background-color: #101522;
+  height: 100%;
+`;
 
-  `
+export const FooterWrap = styled.div`
+  padding: 50px 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1100px;
+  margin: 0 auto;
+`;
 
-  export const FooterWrap=styled.div`
-   padding: 50px 25px;
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items:center;
-   max-width: 1100px;
-   margin: 0 auto;`
+export const FooterLinksContainer = styled.div`
+  display: flex;
+  justify-content: center;
 
-   export const FooterLinksContainer=styled.div`
-   
-   display:flex;
-   justify-content: center;
-   
-   @media screen and (max-width: 800px){
+  @media screen and (max-width: 800px) {
     padding-top: 32px;
-   }`
+  }
+`;
 
-   export const FooterLinksWrapper=styled.div`
-   display: flex;
-   @media screen and (max-width: 800px){
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+  @media screen and (max-width: 800px) {
     flex-direction: column;
+  }
+`;
 
-   }`
+export const FooterLinkTitle = styled.h1`
+  color: #fff;
+  font-size: 2.3vw;
+  font-weight: 4px;
+  font-family: "Montserrat";
+  margin-bottom: 30px;
+`;
+export const SocialMedia = styled.div`
+  max-width: 1000px;
+  width: 100%;
+`;
 
-      
-   export const FooterLinkTitle=styled.h1`
-   color:#fff;
-   font-size: 2.3vw;
-   font-weight:4px;
-   font-family:"Montserrat";
-   margin-bottom: 30px;
-
-   `
-export const SocialMedia=styled.div`
-
-max-width:1000px;
-width:100%;
-
-`
-
-   
-
-
-
-export const SocialIcons=styled.div`
-display:flex;
-justify-content: space-between;
-align-items:center;
-width:25vw;
-`
-
+export const SocialIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 25vw;
+`;
