7f2161131b8a0e10da3a70c7a021c41312ed905e
Aitchessbee <bediharsiddak@gmail.com>
2022-10-18 00:15:13 +0530

    resolved conflicts

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -95,14 +95,15 @@ exports.signup = async (req, res, next) => {
       hard,
     ]);
 
+    // console.log(easyQuestions)
+
     const assignedQuestions = [
       ...shuffleArray(easyQuestions.map((el) => el.questionNumber)).slice(0, 5),
-      ...shuffleArray(mediumQuestions.map((el) => el.questionNumber)).slice(
-        0,
-        4
-      ),
+      ...shuffleArray(mediumQuestions.map((el) => el.questionNumber)).slice(0, 4),
       ...shuffleArray(hardQuestions.map((el) => el.questionNumber)).slice(0, 3),
     ];
+
+    // console.log(assignedQuestions)
 
     // GENERATE OTP TO SAVE AS PASSWORD
     const otp = `${Math.floor(Math.random() * 8999) + 1000}`;
