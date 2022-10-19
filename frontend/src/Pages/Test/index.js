10b5b6fbfafb866bab3bb971202fb213e79df366
sanya-mahajan <sanyamahajan08@gmail.com>
2022-10-19 21:00:46 +0530

    page leave alert

--- frontend/src/Pages/Test/index.js
+++ frontend/src/Pages/Test/index.js
@@ -2,8 +2,23 @@ import LeftAside from "./Components/LeftAside"
 import Main from "./Components/Main"
 import RightAside from "./Components/RightAside"
 import styles from "./Style/test.module.css"
+import { useEffect,useState } from 'react';
+import $ from 'jquery';
+
 
 function Test(){
+    
+const [cheatStrikes, setCheatStrikes] = useState(0);
+
+$(window).blur(function() {
+   // alert("Warning! You are being monitored \n cheat strikes="+{cheatStrikes}+"/3");
+   // console.log({cheatStrikes});
+  //  setCheatStrikes(cheatStrikes+1);
+  alert("Warning! You are being monitored ");
+
+
+});
+
     return (
         <div className={styles.testPage}>
             <div className={styles.testLeftAside}>
