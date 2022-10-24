9d1247196a686bcd80fc29d08ea08c4713ecfecf
tijil <tijilmalhotra@gmail.com>
2022-10-24 13:31:42 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Test from "./Pages/Test"
 import Signup from "./Pages/SignUp";
 // import Login from "./Pages/Login"
-import LoginRedirect from "./Pages/LoginRedirect"
+import Login from "./Pages/Login"
 import Home from "./Pages/Home";
 import Instructions from "./Pages/Instructions";
 
@@ -13,8 +13,8 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
-          <Route path="/login" element={<LoginRedirect />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/signup" element={<Signup/>}></Route>
           <Route path="/instructions" element={<Instructions />}></Route>
          // <Route path="/test" element={<Test />}></Route>
 
